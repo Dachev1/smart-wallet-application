17aaec6bb504239077f4e73d75b01bd2d28bef17
Ivan Dachev <dacheww13@gmail.com>
2025-01-14 19:55:30 +0200

    Created user entity

---
+++ src/main/java/app/user/model/User.java
@@ -0,0 +1,60 @@
+package app.user.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String profilePicture;
+
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Country country;
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+    @Column(nullable = false)
+    private LocalDateTime createdOn;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedOn;
+
+    @OneToMany(fetch = FetchType.EAGER,mappedBy = "user")
+    private List<Subscription> subscriptions;
+
+    @OneToMany(fetch = FetchType.EAGER,mappedBy = "user")
+    private List<Wallet> wallets;
+}
